950d2624db32b6e6bbb2236e9428d5c41bb84bbc
alexs008 <008alexs@gmail.com>
2019-05-27 14:45:46 +0300

    Added several functions

--- sandbox/src/main/java/ua/mytest/MyFirstProgram.java
+++ sandbox/src/main/java/ua/mytest/MyFirstProgram.java
@@ -3,12 +3,26 @@ package ua.mytest;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    String somebody = "World";
-    System.out.println("Hello, " + somebody + "!");
+    hello("World");
+    hello("user");
 
-    double l = 8.0;
-    double s = l * l;
-    System.out.println("Square area with side " + l + " = " + s);
+    double l = 5;
+    System.out.println("Square square" + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Square rectangle " + a + " and " + b + " = " + area(a, b));
+
+  }
+
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+  public static double area(double len){
+    return len * len;
+  }
+  public static double area(double a, double b){
+    return a * b;
   }
 
 }
